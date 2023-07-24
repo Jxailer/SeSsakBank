395393906336515019039ba1fa2a564a9e54541d
hyem1111 <gomasatang24@gmail.com>
2023-07-24 17:01:50 +0900

    게임개발 ver.3.2 (메인에서 게임으로 이동 & 레벨 표시 & 프로그래스바 일정 단위 증가 및 초기화 로직 추가)

--- app/src/main/java/com/example/codevalley/MainActivity.java
+++ app/src/main/java/com/example/codevalley/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.codevalley;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.codevalley.game.Game_nameChange;
+import com.example.codevalley.game.PlantGame;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton gameButton = (ImageButton) findViewById(R.id.gameButton);
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlantGame.class);
+                startActivity(intent);
+            }
+        });
 
     }
 };
--- app/src/main/java/com/example/codevalley/game/PlantGame.java
+++ app/src/main/java/com/example/codevalley/game/PlantGame.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -11,13 +12,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.codevalley.MainActivity;
 import com.example.codevalley.R;
 
 
 public class PlantGame extends AppCompatActivity {
     private int progress_num = 0;
     private int countFertilizer = 21; // 여기에 이제 DB에서 보유 갯수 가져와야 함.
-    private int countWater = 0; // 여기에 이제 DB에서 보유 갯수 가져와야 함.
+    private int countWater = 3; // 여기에 이제 DB에서 보유 갯수 가져와야 함.
     private int countSynthesis = 5; // 여기에 이제 DB에서 보유 갯수 가져와야 함.
     private int countLevel = 1;
 
@@ -30,6 +32,7 @@ public class PlantGame extends AppCompatActivity {
 
         TextView tv_name = (TextView) findViewById(R.id.textView);
         Button btn_nameChange = (Button) findViewById(R.id.btn_nameChange);
+        ImageButton homeButton = (ImageButton) findViewById(R.id.homeButton);
         Button btn_fertilizer = (Button) findViewById(R.id.btn_fertilizer);
         Button btn_water = (Button) findViewById(R.id.btn_water);
         Button btn_synthesis = (Button) findViewById(R.id.btn_synthesis);
@@ -52,6 +55,15 @@ public class PlantGame extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PlantGame.this, Game_nameChange.class);
+                startActivity(intent);
+            }
+        });
+
+        // 하단바 홈버튼 눌렀을 시
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PlantGame.this, MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -90,11 +102,10 @@ public class PlantGame extends AppCompatActivity {
                         }
                     }
                     else {
-                        Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
+                        tv_countFertilizer.setText(0);
                     }
                 }catch (Exception e){
-                    tv_countFertilizer.setText(0);
-                    Toast.makeText(getApplicationContext(),"잘못된 접근입니다.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -102,26 +113,29 @@ public class PlantGame extends AppCompatActivity {
         btn_water.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                countWater--;
-//                tv_countWater.setText(countWater+"");
-                progress_num = progress_num + 1;
+               progress_num = progress_num + 1;
+                if (progress_num % 20 == 0){
+                    countLevel++;
+                    tv_countLevel.setText(countLevel+"");
+                    progress_num = 0;
+                }
                 try {
-                    if (countWater >= 0){
+                    if (countWater > 0){
                         countWater--;
                         tv_countWater.setText(countWater+"");
-                        if (progress_num >= 200  && progress_num < 400){
+                        if (countLevel >= 10 && countLevel < 20){
                             imv_growingPlant.setImageResource(R.drawable.ssessak);
                         }
-                        else if (progress_num >= 400  && progress_num < 600){
+                        else if (countLevel >= 20  && countLevel < 30){
                             imv_growingPlant.setImageResource(R.drawable.small_tree);
                         }
-                        else if (progress_num >= 600  && progress_num < 800){
+                        else if (countLevel >= 30  && countLevel < 40){
                             imv_growingPlant.setImageResource(R.drawable.big_tree);
                         }
-                        else if (progress_num >= 800  && progress_num <= 999){
+                        else if (countLevel >= 40  && countLevel <= 50){
                             imv_growingPlant.setImageResource(R.drawable.big_fruittree);
                         }
-                        else if (progress_num < 0 || progress_num > 999){
+                        else if (countLevel < 0 || countLevel > 50){
                             Toast.makeText(getApplicationContext(),"범위를 초과했습니다.",Toast.LENGTH_SHORT).show();
                         }
                         else{
@@ -130,36 +144,38 @@ public class PlantGame extends AppCompatActivity {
                     }
                     else {
                         tv_countWater.setText(0);
-                        Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
                     }
                 }catch (Exception e){
-                    Toast.makeText(getApplicationContext(),"잘못된 접근입니다.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
         btn_synthesis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                countSynthesis--;
-//                tv_countSynthesis.setText(countSynthesis+"");
                 progress_num = progress_num + 3;
+                if (progress_num % 20 == 0){
+                    countLevel++;
+                    tv_countLevel.setText(countLevel+"");
+                    progress_num = 0;
+                }
                 try {
-                    if (countSynthesis >= 0){
+                    if (countSynthesis > 0){
                         countSynthesis--;
                         tv_countSynthesis.setText(countSynthesis+"");
-                        if (progress_num >= 200  && progress_num < 400){
+                        if (countLevel >= 10 && countLevel < 20){
                             imv_growingPlant.setImageResource(R.drawable.ssessak);
                         }
-                        else if (progress_num >= 400  && progress_num < 600){
+                        else if (countLevel >= 20  && countLevel < 30){
                             imv_growingPlant.setImageResource(R.drawable.small_tree);
                         }
-                        else if (progress_num >= 600  && progress_num < 800){
+                        else if (countLevel >= 30  && countLevel < 40){
                             imv_growingPlant.setImageResource(R.drawable.big_tree);
                         }
-                        else if (progress_num >= 800  && progress_num <= 999){
+                        else if (countLevel >= 40  && countLevel <= 50){
                             imv_growingPlant.setImageResource(R.drawable.big_fruittree);
                         }
-                        else if (progress_num < 0 || progress_num > 999){
+                        else if (countLevel < 0 || countLevel > 50){
                             Toast.makeText(getApplicationContext(),"범위를 초과했습니다.",Toast.LENGTH_SHORT).show();
                         }
                         else{
@@ -168,10 +184,9 @@ public class PlantGame extends AppCompatActivity {
                     }
                     else {
                         tv_countSynthesis.setText(0);
-                        Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
                     }
                 }catch (Exception e){
-                    Toast.makeText(getApplicationContext(),"잘못된 접근입니다.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"보유한 아이템이 없습니다.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
