47042f52696c998c82e998aed1500d0ba779d4ef
SUA <desertsangel@gmail.com>
2023-07-29 15:28:07 +0900

    commit for building transferring data sequence between MainActivity.java and TargetPopupActivity.java. (need to build refresh UI sequence after setting another target)

--- app/src/main/java/com/example/codevalley/MainActivity.java
+++ app/src/main/java/com/example/codevalley/MainActivity.java
@@ -1,24 +1,100 @@
 package com.example.codevalley;
 
+import static com.example.codevalley.TargetPopupActivity.context_TargetPopup;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import com.example.codevalley.wishStore.store_confirm;
 import com.example.codevalley.wishStore.store_main;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Context context_Main;
+    public TextView target; // 캘린더 위에 표시되는 목표 내용 변수
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context_Main = this;
+
+        target = (TextView)findViewById(R.id.targetButton);
+
     }
-    public void gotoWishstore(View v){
-        Intent intent = new Intent(getApplicationContext(), store_main.class);
+
+
+//    목표 설정 버튼
+    public void targetButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "목표 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TargetPopupActivity.class);
+        intent.putExtra("data", target.getText());
+        startActivity(intent);
+        target.setText("변경중 ..."); // 여기에서 targetButton의 text가 변경되는 것은 확인함.
+        finish();
+    }
+
+    //목표 설정 내용 변경하기
+    protected void targetChange(String result){
+        target.setText(result);
+        String targetTest = ((MainActivity)MainActivity.context_Main).target.getText().toString();
+    }
+
+
+
+
+//    정보바 버튼 클릭
+    public void monthButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "월 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+    public void stampButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "스템프 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, StampPopupActivity.class);
         startActivity(intent);
     }
+
+    public void statisticsButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "통계 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+    //캘린더 버튼 눌림
+    public void dayButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "날짜 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, DailyRecordPopupActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivity(intent);
+    }
+
+//    하단 네비게이션 바 버튼 클릭
+    public void homeButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "홈 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void wishstoreButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "소원궈 상점 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), store_main.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void mypageButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "마이페이지 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void plantgameButtonClicked(View v){
+        Toast.makeText(MainActivity.this, "식물키우기 게임 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+
+
 };
--- app/src/main/java/com/example/codevalley/wishStore/store_main.java
+++ app/src/main/java/com/example/codevalley/wishStore/store_main.java
@@ -3,7 +3,11 @@ package com.example.codevalley.wishStore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.codevalley.MainActivity;
 import com.example.codevalley.R;
+import com.example.codevalley.TargetPopupActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,5 +27,34 @@ public class store_main extends AppCompatActivity {
     public void hamburgerMenu_Click(View v){
 
     }
+    public void wishAddButtonClicked(View v){
+        Toast.makeText(store_main.this, "소원추가 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TargetPopupActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivityForResult(intent, 1);
+    }
+
+
+//    네비게이션 하단바 버튼클릭 이벤트
+    public void homeButtonClicked(View v){
+        Toast.makeText(store_main.this, "홈 버튼 눌림.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void wishstoreButtonClicked(View v){
+        Toast.makeText(store_main.this, "소원권 상점 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void mypageButtonClicked(View v){
+        Toast.makeText(store_main.this, "마이페이지 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void plantgameButtonClicked(View v){
+        Toast.makeText(store_main.this, "식물키우기 게임 버튼 눌림.", Toast.LENGTH_SHORT).show();
+    }
+
+
 
 }
