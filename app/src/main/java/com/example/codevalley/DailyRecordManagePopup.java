3bba64b4c3cf80001e91e3331f1b11d1e63e1104
Inseo Kang <JacksonXailer@gmail.com>
2023-08-17 16:48:40 +0900

    developed stampboard Activity (buttons of RecordCreate are updated)

--- app/src/main/java/com/example/codevalley/DailyRecordManagePopup.java
+++ app/src/main/java/com/example/codevalley/DailyRecordManagePopup.java
@@ -14,11 +14,6 @@ public class DailyRecordManagePopup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dailyrecordpopup);
-
-
-
-
-
     }
 
     public void recordCreateButtonClicked(View v){
