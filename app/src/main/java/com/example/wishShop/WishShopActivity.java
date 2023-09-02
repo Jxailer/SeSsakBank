b01693f980676b34ffa6dd6d176d0aceec559d4f
hyerim <gomasatang24@naver.com>
2023-09-02 14:18:02 +0900

    어른 소원관리샵 ver.2 (소원권 업로드 UI추가 수정)

--- app/src/main/java/com/example/wishShop/WishShopActivity.java
+++ app/src/main/java/com/example/wishShop/WishShopActivity.java
@@ -5,15 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.example.codevalley.MainActivity;
 import com.example.codevalley.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
 
 public class WishShopActivity extends AppCompatActivity {
 
@@ -24,7 +18,7 @@ public class WishShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_shop);
 
-        fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.back);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
