a52cbad3d6fa0fc5adcbca7554657038b56ccf39
wjddbs1714 <wjddbs1714@gmail.com>
2023-10-17 00:05:56 +0900

    어린이페이지 도장-소원권/아이템 교환

--- app/src/main/java/com/example/codevalley/wishStore/store_confirm.java
+++ app/src/main/java/com/example/codevalley/wishStore/store_confirm.java
@@ -22,27 +22,26 @@ import java.util.Map;
 
 public class store_confirm extends AppCompatActivity {
     DatabaseReference childRef = FirebaseDatabase.getInstance().getReference("users").child("33@naver,com");
+    DatabaseReference adultRef = FirebaseDatabase.getInstance().getReference("wishManage").child("33@naver,com");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.confirmactivity_store);
     }
 
+    //"네" 버튼 선택 시
     public void rewardConfirm_yes(View v){
         Intent get_intent = getIntent();
+        String wish_title = get_intent.getStringExtra("wish_title");
         Integer stamp_price = get_intent.getIntExtra("stamp_price", 0);
-        if(ur_stamp >= stamp_price){
-            Integer urStamp = ur_stamp - stamp_price;
 
-            Map<String, Object> wishUpdates = new HashMap<>();
-            wishUpdates.put("stamp", urStamp);
-            childRef.updateChildren(wishUpdates);
+        if(ur_stamp >= stamp_price){
+            upDateWishList(wish_title, stamp_price);
 
             Toast.makeText(getApplicationContext(), "구매확인 버튼을 눌렀어요.", Toast.LENGTH_LONG).show();
             Intent yesIntent = new Intent(getApplicationContext(), store_complete.class);
             startActivity(yesIntent);
             finish();
-
         }else{
             Toast.makeText(getApplicationContext(), "도장 개수가 모자라요!", Toast.LENGTH_LONG).show();
             Intent failIntent = new Intent(getApplicationContext(), store_main.class);
@@ -50,14 +49,23 @@ public class store_confirm extends AppCompatActivity {
             finish();
         }
     }
-//    public void wishDelete(String key){
-//        adultRef.child(key).removeValue();
-//    }
 
+    //"아니오" 버튼 선택 시
     public void rewardConfirm_no(View v){
         Toast.makeText(getApplicationContext(), "구매를 취소했어요.", Toast.LENGTH_LONG).show();
         Intent noIntent = new Intent(getApplicationContext(), store_main.class);
         startActivity(noIntent);
         finish();
+    }
+
+    //소원권 리스트 업데이트
+    public void upDateWishList(String wishTitle, Integer stampPrice){
+        Integer urStamp = ur_stamp - stampPrice;
+        //도장 차감
+        Map<String, Object> wishUpdates = new HashMap<>();
+        wishUpdates.put("stamp", urStamp);
+        childRef.updateChildren(wishUpdates);
+        //교환한 소원권은 삭제
+        adultRef.child(wishTitle).removeValue();
     }
 }
