87e109cf61dded5c68f178be2617732f4391b58a
wjddbs1714 <wjddbs1714@gmail.com>
2023-11-19 02:13:36 +0900

    관리자 페이지 가입자 수 그래프

--- app/src/main/java/com/example/codevalley/admin/AdminActivity.java
+++ app/src/main/java/com/example/codevalley/admin/AdminActivity.java
@@ -65,43 +65,43 @@ public class AdminActivity extends AppCompatActivity {
                     else{
                         createDay = create_date.substring(6, 8); //뒤에서 두 자리 남기기 -> 11, 12, ...
                     }
-                    arrayDay.add(createDay);
+                    arrayDay.add(createDay);  // -> 사용자들의 가입 날짜만 저장됨 -> (2일에 아무도 가입하지 않았으면 2는 들어가지 않음)
                 }
 
                 //통계를 위해 1일부터 현재 일자에 대한 가입자 수 배열 생성
                 Integer toDay = Integer.valueOf(getTodayDate()); // 오늘 일자 구하기
                 for(Integer i=0; i<=toDay; i++){  // 0일부터 오늘까지
-                    Integer cnt=0;
-                    for(Integer j=0; j< arrayDay.size(); j++){  //파베에서 읽어온 가입 날짜를 저장한 배열 0번째부터 끝까지
+                    Integer cnt=0;  // 해당 날짜에 가입한 사람 수를 세기 위해
+                    for(Integer j=0; j< arrayDay.size(); j++){  //arrayDay 배열 0번째부터 끝까지
                         Integer date = Integer.valueOf((String) arrayDay.get(j));  //배열에 저장된 가입 날짜는 문자이므로
-                        if(i == date){  //날짜와 '가입날짜'가 같으면
+                        if(i == date){  // 날짜와 '가입날짜'가 같으면
                             cnt += 1;
                         }
                     }
-                    arrayAnalytics.add(cnt);   //리스트에 추가
+                    arrayAnalytics.add(cnt);   // -> 이번달의 일 수(11월은 1일부터 30일까지 있음)만큼 들어있고 가입한 사람 수가 해당 '일'자(인덱스와 동일)에 들어가있음
                 }
 
                 // 지난주 가입자수 담기(통계를 7일씩만 보여줄 것이기 때문)
                 ArrayList list1 = new ArrayList<>();
                 for(Integer j=13; j>6; j--){
-                    Integer data = (Integer) arrayAnalytics.get(toDay - j);
-                    list1.add(data);
+                    Integer data = (Integer) arrayAnalytics.get(toDay - j);  // 만약 toDay(오늘)이 19일이면 (19-13)일부터 (19-7)일의 데이터
+                    list1.add(data);  // 최종적으로 7개의 데이터가 들어감(날짜가 갱신되면 데이터도 갱신됨)
                 }
                 // 이번주 가입자수 담기
                 ArrayList list2 = new ArrayList<>();
                 for(Integer j=6; j>-1; j--){
-                    Integer data = (Integer) arrayAnalytics.get(toDay - j);
+                    Integer data = (Integer) arrayAnalytics.get(toDay - j);  // 만약 toDay가 19일이면 (19-6)일부터 (19-0)일의 데이터
                     list2.add(data);
                 }
-                //y축에 넣을 데이터 entry 생성
+                //차트에 넣을 entry 데이터 생성
                 for(Integer i=0; i<7; i++){
-                    arrayLastWeek.add(new Entry(i, ((Number)list1.get(i)).floatValue()));
+                    arrayLastWeek.add(new Entry(i, ((Number)list1.get(i)).floatValue()));  // Entry에는 float형만 들어가는데 Integer를 Number로 바꿔야 float형으로 변환할 수 있음(굳이?)
                     arrayThisWeek.add(new Entry(i, ((Number)list2.get(i)).floatValue()));
                 }
                 //x축의 범위 설정을 위한 리스트 생성
                 for(Integer i=(toDay-6); i<=toDay; i++){
                     String j = String.valueOf(i);
-                    arrayXRange.add(j);
+                    arrayXRange.add("11-" + j);
                 }
                 System.out.println(arrayXRange);
 
@@ -145,24 +145,23 @@ public class AdminActivity extends AppCompatActivity {
                 xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);  // x축은 아래에 위치
                 xAxis.setDrawAxisLine(false);  // x축 선 지우기
                 xAxis.setDrawGridLines(false);  // 격자무늬 지우기
-                xAxis.setTextSize(10f);  // x축 글자 크기
                 xAxis.setValueFormatter(new IndexAxisValueFormatter(arrayXRange));  // x축 범위 설정
                 //왼쪽 y축 설정
                 YAxis yLAxis = lineChart.getAxisLeft();
                 yLAxis.setTextSize(10f);
                 yLAxis.setDrawAxisLine(false);
                 yLAxis.setAxisMinimum(0f);  // y축 최솟값
-                yLAxis.setGranularity(0.5f);  // y축 간격
+                yLAxis.setGranularity(1f);  // y축 간격
                 //오른쪽 y축 설정
                 YAxis yRAxis = lineChart.getAxisRight();
                 yRAxis.setDrawLabels(false);  // 레이블 지우기
-                yRAxis.setDrawAxisLine(false);
-                yRAxis.setDrawGridLines(false);
+                yRAxis.setDrawAxisLine(false);  // 축 선 지우기
+                yRAxis.setDrawGridLines(false);  // 그리드 선 지우기
 
-                lineChart.setDoubleTapToZoomEnabled(false);
-                lineChart.setDrawGridBackground(false);
+                lineChart.setDoubleTapToZoomEnabled(false);  // 확대 안됨
+                lineChart.setDrawGridBackground(false);  // 격자배경 지우기
 
-                lineChart.invalidate();
+                lineChart.invalidate();  // 차트 다시 받아오기
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
