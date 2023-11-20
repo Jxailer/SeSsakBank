f4affc9db3da05abe8cc5a5fc87885fc04652108
YuRim0420 <yuwol0420@gmail.com>
2023-11-21 02:40:31 +0900

    부모 마이페이지 UI

--- app/src/main/java/com/example/adult/profile/Profile.java
+++ app/src/main/java/com/example/adult/profile/Profile.java
@@ -1,23 +1,56 @@
 package com.example.adult.profile;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.adult.HomeActivity;
 import com.example.adult.post.BoardListActivity;
+import com.example.codevalley.MainActivity;
 import com.example.codevalley.R;
+import com.example.codevalley.game.GameStart1;
+import com.example.codevalley.game.PlantGame;
+import com.example.codevalley.wishStore.store_main;
 import com.example.wishShop.WishShopActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Switch onoffswitch2 = findViewById(R.id.push_onOff2);
+        onoffswitch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheked) {
+                if(isCheked) {
+                    Toast.makeText(getApplicationContext(), "알림이 설정되었습니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "알림이 해제되었습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
+
 
     //    하단 네비게이션 바 버튼 클릭
     public void homeButtonClicked(View v){
@@ -39,3 +72,4 @@ public class Profile extends AppCompatActivity {
         startActivity(communityIntent);
     }
 }
+
